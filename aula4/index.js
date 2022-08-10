99faea8575eec77547b982be5d82adb2668b8347
Fernando <fernando.bbosco@gmail.com>
2022-08-10 17:52:07 -0300

    resolucao aula5

--- aula4/index.js
+++ aula4/index.js
@@ -1,12 +1,32 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
+    for (let i of data) {
+      const ul = document.getElementById("lista");
+      const li = document.createElement("li");
+      li.innerText = i.name;
+      ul.appendChild(li);
+    }
+  });
+
+
+async function getUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
 
     for (let i of data) {
-        const ul = document.getElementById('lista')
-        const li = document.createElement('li')
-        li.innerText = i.name
-        ul.appendChild(li)
+      const ul = document.getElementById("lista");
+      const li = document.createElement("li");
+      li.innerText = i.name;
+      ul.appendChild(li);
     }
 
-  });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    console.log('acabou!')
+  }
+}
+
+getUsers();
